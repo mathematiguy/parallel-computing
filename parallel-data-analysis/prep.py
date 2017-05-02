49ef2359766e0cdbe872b8ad39999be00f429635
mathematiguy <calebjdmoses@gmail.com>
2017-05-02 21:07:27 +1200

    modified prep.py to work with python 3.6

--- parallel-data-analysis/prep.py
+++ parallel-data-analysis/prep.py
@@ -1,25 +1,28 @@
 import os
 import shutil
 from glob import glob
+from fakestockdata import generate_stocks
 import pandas as pd
 import json
-from fakestockdata import generate_stocks
-from concurrent.futures import ProcessPoolExecutor
 
-here = os.path.dirname(__file__)
+def get_data_dir():
+	here = os.path.dirname(__file__)
+	data = os.path.join(here, 'data')
+	return data
 
-data = os.path.join(here, 'data')
-if not os.path.exists(data):
-    os.mkdir(data)
+def make_data_dir():
+	data = get_data_dir()
+	if not os.path.exists(data):
+	    os.mkdir(data)
 
-
-minute = os.path.join(data, 'minute')
-if not os.path.exists(minute):
-    os.mkdir(minute)
-    generate_stocks(freq=pd.Timedelta(seconds=120),
-                    start=pd.Timestamp('2010-01-01'),
-                    directory=minute)
-
+def make_minute_data():
+	data = get_data_dir()
+	minute = os.path.join(data, 'minute')
+	if not os.path.exists(minute):
+		os.mkdir(minute)
+		generate_stocks(freq=pd.Timedelta(seconds=120),
+		                start=pd.Timestamp('2010-01-01'),
+		                directory=minute)
 
 def convert_to_json(d):
     filenames = sorted(glob(os.path.join(d, '*')))[-365:]
@@ -31,11 +34,24 @@ def convert_to_json(d):
                 f.write(os.linesep)
     print("Finished JSON: %s" % d)
 
+def write_json_files():
+	data = get_data_dir()
+	js = os.path.join(data, 'json')
+	if not os.path.exists(js):
+		from concurrent.futures import ProcessPoolExecutor
+		minute = os.path.join(data, 'minute')
+		os.mkdir(js)
+		directories = sorted(glob(os.path.join(minute, '*')))
 
-js = os.path.join(data, 'json')
-if not os.path.exists(js):
-    os.mkdir(js)
-    directories = sorted(glob(os.path.join(minute, '*')))
+		e = ProcessPoolExecutor()
+		list(e.map(convert_to_json, directories))
 
-    e = ProcessPoolExecutor()
-    list(e.map(convert_to_json, directories))
+
+def main():
+	make_data_dir()
+	make_minute_data()
+	write_json_files()
+
+
+if __name__ == "__main__":
+	main()
